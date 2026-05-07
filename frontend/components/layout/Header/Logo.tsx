29a6409f309cac9f888ebbf21b45e7b32b203c9d
SarBajit07 <Sarbajeet4604@gmail.com>
2026-05-07 17:02:56 +0545

    logo optimized

--- frontend/components/layout/Header/Logo.tsx
+++ frontend/components/layout/Header/Logo.tsx
@@ -3,13 +3,21 @@ import Image from 'next/image';
 
 export function Logo() {
   return (
-    <Link href="/" className="group flex items-center transition-transform duration-300 ease-out hover:-translate-y-0.5">
+    <Link href="/" className="group flex items-center gap-2.5 transition-transform duration-300 ease-out hover:-translate-y-0.5">
+      <Image
+        src="/24ghantalogo.jpg"
+        alt="24 Ghanta icon"
+        width={48}
+        height={48}
+        className="h-10 w-10 rounded-md object-contain"
+        priority
+      />
       <Image
         src="/24 GHANTA BLACK.png"
         alt="24 Ghanta"
         width={160}
         height={45}
-        className="h-9 w-auto object-contain"
+        className="h-8 w-auto object-contain"
         priority
       />
     </Link>
